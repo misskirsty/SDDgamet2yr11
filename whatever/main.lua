59ff5bdd3b51a924836fec6f10207a42c03ac48c
misskirsty <miss.k.kirsty@gmail.com>
2017-05-08 18:35:17 +1000

    Update main.lua

--- whatever/main.lua
+++ whatever/main.lua
@@ -1,10 +1,16 @@
 
 function love.load()
-	love.window.setMode(800, 800, {resizable=true,vsync=true})
+	love.window.setMode(1600, 900, {resizable=true,vsync=true})
 	width = 160
 	height = 100
 	image = love.graphics.newImage('farm.jpg')
 	playerimage = love.graphics.newImage('Stick_figure.png')
+  playergun = love.graphics.newImage('gun.png')
+	playerx = 400
+	playery = 600
+  playergunx = playerx + 140
+  scalefactorx = 1
+  scalefactory = scalefactorx
 
 end
 
@@ -14,6 +20,18 @@ function testmouse()
 
 end
 
+function love.keypressed(key)
+	if key == 'a' then
+		playerx = playerx - 20
+    scalefactorx = -1
+    playergunx = playerx - 140
+  elseif key == 'd' then
+		playerx = playerx + 20
+    scalefactorx = 1
+    playergunx = playerx + 140
+  end
+end
+
 
 function drawplayer()
 
@@ -21,14 +39,12 @@ end
 
 
 function love.draw()
-
 	love.graphics.draw( image,0,0) --image, x, y, r, sx, sy, ox, oy, kx, ky
-	love.graphics.draw(playerimage, 400, 600)
+	love.graphics.draw(playerimage, playerx, playery, 0, scalefactorx, scalefactory)
+  love.graphics.draw(playergun, playergunx, playery, 0, scalefactorx, scalefactory)
 end
 
 function love.update()
 
 end
-
-
 
